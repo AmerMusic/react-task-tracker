24fe9b106b718251b5493b8d50bc3c3d99776a68
AmerMusic <50263083+AmerMusic@users.noreply.github.com>
2021-07-11 15:59:54 +0200

    React-Task-Tracker

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from '../src/components/Header'
+import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
+
+import { useState } from 'react'
+
 
 function App() {
+  const [showAddTask, setShowAddTask]=useState(false)
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Doctors Appointment',
+      day: 'July 10th at 3:30pm',
+      reminder: true,
+    },
+
+    {
+      id: 2,
+      text: 'Meeting at School',
+      day: 'July 11th at 1:30pm',
+      reminder: true,
+    },
+
+    {
+      id: 3,
+      text: 'Food Shopping',
+      day: 'July 10th at 10:30am',
+      reminder: false,
+    },
+  ])
+
+  // Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+
+  }
+
+  //Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task))
+  }
+
+  //Add Task
+  const addTask=(task)=>{
+
+    const id=Math.floor(Math.random()*1000) +1
+    const newTask={id, ...task}
+    setTasks([...tasks, newTask])
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header onAdd={()=>setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
+      {showAddTask && <AddTask onAdd={addTask} />}
+      { /* if there is no task, then print a message */}
+      {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />) : ('No Task to Show')}
+      
     </div>
   );
 }
